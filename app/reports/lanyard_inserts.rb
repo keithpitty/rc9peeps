7fac95396c0fdfa1de194e3b209a820909dad65f
Jason Crane <jason@codespike.com>
2011-06-06 07:24:09 +1000

    new font, added notes pages. Still need to rejig the sup page, and annotate the map

--- app/reports/lanyard_inserts.rb
+++ app/reports/lanyard_inserts.rb
@@ -1,18 +1,18 @@
 class LanyardInserts < Prawn::Document
 
   def initialize(options = {}, &block)
-    super page_size: 'A5', page_layout: :landscape
+    super page_size: 'A5', page_layout: :landscape, :margin => 10
   end  
 
   def to_pdf
     Peep.all.each_with_index do |peep, index|
       @peep = peep
       # Print on 2 pages double-sided for folding
-      define_grid(columns: 2, rows: 1)
+      define_grid(columns: 2, rows: 1, column_gutter: 20)
       page_four
       page_one
       start_new_page
-      define_grid(columns: 2, rows: 1)
+      define_grid(columns: 2, rows: 1, column_gutter: 20)
       page_two
       page_three
       start_new_page unless index == Peep.count - 1
@@ -24,25 +24,32 @@ class LanyardInserts < Prawn::Document
     def page_one
       page_1 = grid(0, 1)
       bounding_box page_1.top_left, width: page_1.width, height: page_1.height do
-#        stroke_bounds
-        font "Helvetica"
+ #       stroke_bounds
+        font "#{RAILS_ROOT}/public/images/KOMIKAX_.ttf"
         font_size 60
         text " "
-        font_size 50
-        text @peep.first_name, align: :center
-        font_size 14
-        text @peep.last_name, align: :center
-        font_size 36
+        font_size 44
+ #       fill_color "%06x" % (rand * 0xffffff)
+        text @peep.first_name, :indent_paragraphs => 10
+        font_size 16
+        text @peep.last_name, :indent_paragraphs => 10
+ #       fill_color "#000000"
+        font_size 90
         text " "
         font_size 24
-        text @peep.twitter_handle, align: :center
+        unless @peep.twitter_handle.empty?
+          text @peep.twitter_handle, :indent_paragraphs => 10
+        end
+        logo = "#{Rails.root}/public/images/logo.png"
+        image logo, at: [120, 115], width: 150 
+        
       end
     end
     
     def page_two
       page_2 = grid(0, 0)
       bounding_box page_2.top_left, width: page_2.width, height: page_2.height do
-#        stroke_bounds
+ #       stroke_bounds
         font "Helvetica"
         font_size 32
         text "http://sup"
@@ -69,15 +76,21 @@ class LanyardInserts < Prawn::Document
       page_3 = grid(0, 1)
       bounding_box page_3.top_left, width: page_3.width, height: page_3.height do
 #        stroke_bounds
+        font "Helvetica"
+        font_size 32
+        text "Notes"
+        line_count = 10
+        line_step = ((page_3.height-50)/line_count).to_i
+        (0..page_3.height-50.to_i).step(line_step) {|i| line [0,i],[page_3.width,i];stroke}
       end
     end
     
     def page_four
       page_4 = grid(0, 0)
       bounding_box page_4.top_left, width: page_4.width, height: page_4.height do
-#        stroke_bounds
+ #       stroke_bounds
         map = "#{Rails.root}/public/images/map.png"
-        image map, at: [0, 347.53], width: 261.64, height: 347.53
+        image map, at: [1, 398], width: 275, height: 397
       end
     end
   
