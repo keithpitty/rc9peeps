54d5f33894a4c81d91ea88096c44999b59224bba
Jason Crane <jason@codespike.com>
2011-06-06 23:12:24 +1000

    updated map and other bits

--- app/reports/lanyard_inserts.rb
+++ app/reports/lanyard_inserts.rb
@@ -50,25 +50,54 @@ class LanyardInserts < Prawn::Document
       page_2 = grid(0, 0)
       bounding_box page_2.top_left, width: page_2.width, height: page_2.height do
  #       stroke_bounds
-        font "Helvetica"
-        font_size 32
-        text "http://sup"
+        font "#{RAILS_ROOT}/public/images/KOMIKAX_.ttf"
+        font_size 24
+        text "Map Legend"
+        font "#{RAILS_ROOT}/public/images/DroidSansMono.ttf"
+        font_size 10
         text " "
+        text "A: Conference room south (has projector)"
+        text " "
+        text "B: Conference room north (has projector)"
+        text " "
+        text "C: BBQ Area (casual hacking, chatting)"
+        text " "
+        text "D: Museum (informal chats, no projector)"
+        text " "
+        text "E: Stadium (great for rock band)"
+        text " "
+        text "F: Library under dining room (has projector)"
+        text " "
+        text "G: Grass area (good for outdoor talks)"
+        text " "
+        text " "
+        text " "
+
+        font "#{RAILS_ROOT}/public/images/KOMIKAX_.ttf"
+        font_size 24
+        text "At a glance"
+        font_size 10 
+        text " "
+        
+        font "#{RAILS_ROOT}/public/images/DroidSansMono.ttf"
         font_size 14
         text "Meal Times:"
-        font "Courier"
         font_size 12
-        text "Breakfast: 7:30 - 8:00 am"
-        text "Lunch:    12:00 - 1:00 pm"
-        text "Dinner:    6:30 - 7:30 pm"
+        text " "
+        text "Breakfast:  7:30 - 8:00 am"
+        text "Lunch:     12:00 - 1:00 pm"
+        text "Dinner:     6:30 - 7:30 pm"
         text " "
         font_size 14
-        font "Helvetica"
         text "Organising group contacts:"
-        font "Courier"
         font_size 12
+        text " "
         text "Jason Crane: #{CONFIG['snapperwolf_mobile']}"
         text "Keith Pitty: #{CONFIG['beardedkeith_mobile']}"
+        font_size 9
+        text " "
+        text " "
+        text "Bus for Gold Coast leaves Monday 10am sharp!"
       end
     end
     
@@ -76,8 +105,8 @@ class LanyardInserts < Prawn::Document
       page_3 = grid(0, 1)
       bounding_box page_3.top_left, width: page_3.width, height: page_3.height do
 #        stroke_bounds
-        font "Helvetica"
-        font_size 32
+        font "#{RAILS_ROOT}/public/images/KOMIKAX_.ttf"
+        font_size 24
         text "Notes"
         line_count = 10
         line_step = ((page_3.height-50)/line_count).to_i
