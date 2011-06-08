8b3c56b61f0f750cba249803bf12d628cb8692d2
Jason Crane <jason@codespike.com>
2011-06-08 17:15:16 +1000

    tweak of lanyard text inside pages

--- app/reports/lanyard_inserts.rb
+++ app/reports/lanyard_inserts.rb
@@ -75,29 +75,20 @@ class LanyardInserts < Prawn::Document
 
         font "#{RAILS_ROOT}/public/images/KOMIKAX_.ttf"
         font_size 24
-        text "At a glance"
-        font_size 10 
+        text "At camp"
+        font_size 10
         text " "
-        
         font "#{RAILS_ROOT}/public/images/DroidSansMono.ttf"
-        font_size 14
-        text "Meal Times:"
-        font_size 12
         text " "
-        text "Breakfast:  7:30 - 8:00 am"
-        text "Lunch:     12:00 - 1:00 pm"
-        text "Dinner:     6:30 - 7:30 pm"
+        text "Introduce yourself to two people you don't know"
         text " "
-        font_size 14
-        text "Organising group contacts:"
-        font_size 12
+        text "Thank at least one person who's helped you with projects you've worked on in the past (or is helping you at camp)"
         text " "
-        text "Jason Crane: #{CONFIG['snapperwolf_mobile']}"
-        text "Keith Pitty: #{CONFIG['beardedkeith_mobile']}"
-        font_size 9
+        text "Railscamp is what you make of it, so get involved!"
         text " "
         text " "
-        text "Bus for Gold Coast leaves Monday 10am sharp!"
+        text " "
+        text "Code, Share, Respect"
       end
     end
     
@@ -107,10 +98,34 @@ class LanyardInserts < Prawn::Document
 #        stroke_bounds
         font "#{RAILS_ROOT}/public/images/KOMIKAX_.ttf"
         font_size 24
-        text "Notes"
-        line_count = 10
-        line_step = ((page_3.height-50)/line_count).to_i
-        (0..page_3.height-50.to_i).step(line_step) {|i| line [0,i],[page_3.width,i];stroke}
+        text "Meal Times"
+        font "#{RAILS_ROOT}/public/images/DroidSansMono.ttf"
+        font_size 16
+        text " "
+        text "Breakfast:  7:30 - 8:00 am"
+        text "Lunch:     12:00 - 1:00 pm"
+        text "Dinner:     6:30 - 7:30 pm"
+        text " "
+        text " "
+        text " "
+        font_size 24
+        font "#{RAILS_ROOT}/public/images/KOMIKAX_.ttf"
+        text "Organising group contacts"
+        font "#{RAILS_ROOT}/public/images/DroidSansMono.ttf"
+        font_size 16
+        text " "
+        text "Jason Crane: #{CONFIG['snapperwolf_mobile']}"
+        text "Keith Pitty: #{CONFIG['beardedkeith_mobile']}"
+        text " "
+        text " "
+        text " "
+        text " "
+        text " "
+        text " "
+        text " "
+        text " "
+        font_size 9
+        text "Bus for Gold Coast leaves Monday 10am sharp!"
       end
     end
     
